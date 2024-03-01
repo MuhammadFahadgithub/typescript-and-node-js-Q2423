4ab7b8e079581a8b51ee4bfcd860e76f2216c5b1
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-01 18:25:32 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,40 @@
+// Task 24
+// Test.1.Equality and Inequality Test.
+console.log("Equality test with string: ","Apple" === "Apple");
+
+// Test.2.Equality and Inequality Test.
+console.log("Equality test with string: ",("Apple" as string) != "Orange");
+
+// Tests using the lower case function.
+console.log("Lower Case funtion test: ", "HEllo".toLowerCase() ==="hello");
+
+// Numerical test involving equality
+console.log("Equality test with numbers:", 27 === 27 );
+
+// Numerical test involving inequality
+console.log("Equality test with numbers:",(23 as number) !=27);
+
+// Greater than test
+console.log("Greater than test: ", 10>5);
+
+// Less than test
+console.log ("Less than test: ", 5<10);
+
+// Greater than or less than test
+console.log("Greater than and equal to test: ", 10>=5);
+
+// Less than or equal to test
+console.log ("Less than or equal to test: ", 5<= 10);
+
+// Test using "and" operator
+console.log("And operator test:", 5===5 && 10> 5);
+
+// Test using "Or" operator
+console.log("Or operator test:", 5===5 || false );
+
+// Test whether an item is in a arry
+const fruits :string[]= ['Mango', 'strawberry', 'Banana']
+console.log('Test ""Banana" in the arry:', fruits.includes("Banana"));
+
+// Test whether an item is not in a arry
+console.log('Testing"Apple" is not in Arry:', fruits.includes('Apple'));
